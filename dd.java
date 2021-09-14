d103487298d6a1ba677de838d073d4930e289d63
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-09-14 15:07:08 +0900

    Add files via upload

---
+++ dd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class dd {
+	public static void main(String[] args) {
+		int iTime;
+		int iSecond;
+		int iMinute;
+		int iHour;
+		Scanner in = new Scanner(System.in);
+		System.out.print("정수를 입력하세요:");
+		
+		iTime = in.nextInt();
+		iSecond = iTime % 60;
+		iMinute = (iTime / 60) % 60;
+		iHour = (iTime/60)/60;
+		
+		System.out.print(iTime + "초는" );
+		System.out.print(iHour + "시간" );
+		System.out.print(iMinute + "분" );
+		System.out.print(iSecond + "초입니다." );
+	
+	}
+}
