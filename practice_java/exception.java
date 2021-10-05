e8c28370da2e868a517c665e35dbfba63d863167
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-10-05 16:25:33 +0900

    Add files via upload

---
+++ practice_java/exception.java
@@ -0,0 +1,22 @@
+// 오류 
+// 예외(exception) -> 복구 가능한 덜 심각한 오류
+
+// TRY-CATCH
+
+public class exception{
+	public static void main(String[] args) {
+		try {
+			String[] str = new String[2];
+			for(int i =0;i<3;i++) {
+				str[i] = "Number is" +(i+1);				
+		}
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println("Exception is caught");
+		}
+		finally {
+			System.out.println("Program ended(1)");
+		}
+		System.out.println("Program ended(2)");
+	}
+}
