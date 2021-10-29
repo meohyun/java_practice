01592a3607978645c7561818e11c1add84355c1b
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-10-29 14:31:32 +0900

    Update question_1.java

--- exam_practice/question_1.java
+++ exam_practice/question_1.java
@@ -1,23 +1,31 @@
 public class question_1{
 	public static void main(String[] args) {
 		int i,j;
-		String [][] meohyun = {
-				{"78","48","78","98"},
-				{"99","92","",""},
-				{"29","64","83",""},
-				{"34","78","92","56"}};
+		int sum = 0;
+		int nums = 0;
 		
-		
+		int [][] a = {
+				{78,48,78,98},
+				{99,92},
+				{29,64,83},
+				{34,78,92,56}
+		};
 				
 		for (i=0;i<4;i++) {
-			for (j=0;j<4;j++) {
+			for (j=0;j<a[i].length;j++) {
 				
+				sum += a[i][j];
+				nums += 1;
 		
-				System.out.print(meohyun[i][j] + " ");
+				System.out.print(a[i][j] + " ");
+				
 				
 				}
 			System.out.println();
+			
 			}
+		System.out.println("합은 "+sum+"입니다.");
+		System.out.println("평균은 "+ sum/nums+"입니다.");
 		}
 		
 
